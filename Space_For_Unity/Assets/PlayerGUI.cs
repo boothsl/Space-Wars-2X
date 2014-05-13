ccd5456c5679c4689a1c0fbc8f0df21a528de004
stephanie.booth <stephanie.booth@taurigroup.com>
2014-05-13 08:42:44 -0400

    Added split screen, asteroid field (again), fixed left player w-s movement. Made escape key work for all scenes.

---
+++ Space_For_Unity/Assets/PlayerGUI.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerGUI : MonoBehaviour {
+	public Camera myCamera;
+	public GUIStyle borderGUIStyle;
+
+	void OnGUI() {
+		float width = myCamera.pixelWidth;
+		float height = myCamera.pixelHeight;
+
+		GUI.Box (new Rect (0, 0, width, height), "", borderGUIStyle);
+	}
+}
