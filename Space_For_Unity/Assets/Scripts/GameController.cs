af2879c338c14602e88a82e425dffcaef1845656
marn <martin@asimia.net>
2014-05-17 19:47:42 -0400

    LeagueOfSpace test, bases, minion spawning and epic space battles

---
+++ Space_For_Unity/Assets/Scripts/GameController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Game manager
+/// </summary>
+public class GameController : MonoBehaviour {
+
+	public enum GameState
+	{
+		Active,
+		GameOver,
+	}
+
+	private static GameController _Instance;
+
+	public static GameController Instance {
+		get { return _Instance; }
+	}
+
+	// Game over object(s) to enable on end game
+	public GameObject[] gameOver;
+
+	public GameState state = GameState.Active;
+
+	void Awake() {
+		_Instance = this;
+	}
+
+	public void GameOver()
+	{
+		if (state != GameState.GameOver)
+		{
+			state = GameState.GameOver;
+			foreach (GameObject obj in gameOver)
+			{
+				if (obj != null)
+					obj.SetActive(true);
+			}
+		}
+	}
+}
