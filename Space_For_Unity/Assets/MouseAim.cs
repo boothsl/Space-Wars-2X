fe666482f321ee499a71fbc6e0a0e2537517e7d3
Chris Hull <chris.f.hull@gmail.com>
2014-06-28 18:05:24 -0400

    Changed mouseaim script to be less terrible

--- Space_For_Unity/Assets/MouseAim.cs
+++ Space_For_Unity/Assets/MouseAim.cs
@@ -5,21 +5,41 @@ public class MouseAim : MonoBehaviour {
 	[SerializeField] Transform mouseAimObject;
 	[SerializeField] Texture2D crosshairTexture;
 
-	private GUITexture crosshair;
+	[SerializeField] Vector3 targetDirection; // Normalized direction.
+	[SerializeField] float targetSpeed; //World units.
+	
+	[SerializeField] float projectileSpeed = 200.0f; //World units
+	[SerializeField] float maxDegreeRotation = 30.0f;
+
+	public bool paused;
 	// Use this for initialization
 	void Start () {
 		Screen.showCursor = false;
-		crosshair.texture = crosshairTexture;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		Vector3 mousePos = Input.mousePosition;
-		Vector3 wantedPos = Camera.main.ScreenToWorldPoint (new Vector3 (mousePos.x, mousePos.y, 1000.0f));
-		transform.position = wantedPos;
-		transform.parent.LookAt(wantedPos);
 	}
 
+	// Update is called once per frame
+	void Update () {
+				if (!paused)
+						return;
+			
+				targetDirection = mouseAimObject.transform.forward;
+				targetSpeed = mouseAimObject.rigidbody.velocity.magnitude;
+
+				Vector3 leadDirection = mouseAimObject.position - transform.position;
+				leadDirection += leadDirection.magnitude
+						* targetDirection
+						* targetSpeed
+						/ projectileSpeed;
+				transform.rotation = Quaternion.RotateTowards (transform.rotation,
+		                                              Quaternion.LookRotation (leadDirection, transform.up),
+		                                              maxDegreeRotation);	// Update is called once per frame
+//	void Update () {
+//		Vector3 mousePos = Input.mousePosition;
+//		Vector3 wantedPos = Camera.main.ScreenToWorldPoint (new Vector3 (mousePos.x, mousePos.y, 1000.0f));
+//		transform.position = wantedPos;
+//		transform.parent.position.MoveTowards(transform.position, wantedPos, 1.0f);
+//	}
+		}
 	
 		
 	void OnGUI()	
@@ -28,9 +48,7 @@ public class MouseAim : MonoBehaviour {
 		
 		mousePos.x = Mathf.Clamp(mousePos.x, Screen.width/Screen.width, Screen.width);
 		mousePos.y = Mathf.Clamp(mousePos.y, Screen.height/Screen.height, Screen.height);
-
 		
-
 		GUI.DrawTexture(new Rect( mousePos.x - ((Screen.width / 20) / 2),
 		                      mousePos.y -  ((Screen.width / 20) / 2),
 		                      Screen.width / 20,
@@ -38,5 +56,8 @@ public class MouseAim : MonoBehaviour {
 		                	  crosshairTexture);
 	}
 
+	void OnApplicationFocus(bool focusStatus) {
+			paused = focusStatus;
+		}
 
 }
---
+++ Space_For_Unity/Assets/Scripts/TrackingObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrackingObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 mousePos = Input.mousePosition;
+		Vector3 wantedPos = Camera.main.ScreenToWorldPoint (new Vector3 (mousePos.x, mousePos.y, 1000.0f));
+		transform.position = wantedPos;
+	}
+}
